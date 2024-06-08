db58f6598a8c6eb146d2ae6e68f8a91998710235
weien02 <weien02@gmail.com>
2024-06-08 18:47:33 +0800

    Added edit profile sheet

--- src/components/homepage/Sidebar.tsx
+++ src/components/homepage/Sidebar.tsx
@@ -4,6 +4,18 @@ import { useToast } from "../ui/use-toast";
 import useSignOut from "@/_authentication/hooks/useSignOut";
 import { getFirstname, getUsername } from "@/_authentication/authFunctions";
 import { sidebarLinks } from "@/constants";
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 
 function Sidebar(){
@@ -25,17 +37,48 @@ function Sidebar(){
           <h1 className="h3-bold">ChronoGram</h1>
         </Link>
 
-        <Link to="/profile" className={`flex gap-3 items-center sidebar-link ${pathname === "/profile" ? "  bg-light-2" : ""}`}>
-            <img
-              src={"/assets/glyphs/user.png"}
-              alt="profile"
-              className={'h-14 w-14 rounded-full'}
-            />
-            <div className="flex flex-col">
-              <p className="base-semibold">{getFirstname()}</p>
-              <p className="small-regular text-primary-600">@{getUsername()}</p>
+        <Sheet>
+          <SheetTrigger asChild>
+            <div className="flex items-center gap-3 sidebar-link">
+              <img
+                src={"/assets/glyphs/user.png"}
+                alt="profile"
+                className={'h-14 w-14 rounded-full'}
+              />
+              <div className="flex flex-col">
+                <p className="base-semibold">{getFirstname()}</p>
+                <p className="small-regular text-primary-600">@{getUsername()}</p>
+              </div>
             </div>
-          </Link>
+          </SheetTrigger>
+          <SheetContent side="left" className="bg-light-3 w-[500px]">
+            <SheetHeader>
+              <SheetTitle>Edit profile *Not working!*</SheetTitle>
+              <SheetDescription>
+                Update your profile details.
+              </SheetDescription>
+            </SheetHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="firstName" className="text-left">
+                  First Name
+                </Label>
+                <Input id="name" defaultValue={getFirstname()} className="col-span-3" />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="username" className="text-left">
+                  Username
+                </Label>
+                <Input id="username" defaultValue={getUsername()} className="col-span-3" />
+              </div>
+            </div>
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button type="submit" className="shad-button_primary">Save changes *Does nothing*</Button>
+              </SheetClose>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
 
         <ul className="flex flex-col gap-6">
           {sidebarLinks.map((link) => {
