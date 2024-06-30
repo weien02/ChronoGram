99d9246a556b28d06a3055a34defa7fd11f6c11f
weien02 <weien02@gmail.com>
2024-06-30 21:47:55 +0800

    Added sidebar selected glyphs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,26 +1,31 @@
 export const sidebarLinks = [
     {
         imgURL: "/assets/glyphs/home.png",
+        imgURLSelected: "/assets/glyphs/home_selected.png",
         route: "/",
         label: "Home",
     },
     {
         imgURL: "/assets/glyphs/tunnel.png",
+        imgURLSelected: "/assets/glyphs/tunnel_selected.png",
         route: "/time-tunnel",
         label: "Time Tunnel",
     },
     {
         imgURL: "/assets/glyphs/capsule.png",
+        imgURLSelected: "/assets/glyphs/capsule_selected.png",
         route: "/my-capsules",
         label: "My Capsules",
     },
     {
         imgURL: "/assets/glyphs/shared.png",
+        imgURLSelected: "/assets/glyphs/shared_selected.png",
         route: "/shared-with-me",
         label: "Shared With Me",
     },
     {
         imgURL: "/assets/glyphs/create.png",
+        imgURLSelected: "/assets/glyphs/create_selected.png",
         route: "/create-capsule",
         label: "Create Capsule",
     },
